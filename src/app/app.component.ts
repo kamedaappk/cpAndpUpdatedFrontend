1b54bb0bb1a7a2e2d60d10bc2897ae51f4e06b39
sudomaster00081 <a.jayprasadd0008@gmail.com>
2024-09-05 18:45:27 +0530

    committed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { CommonModule } from '@angular/common';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'; //
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,
+  imports: [RouterOutlet,CommonModule,HomeComponent ],
+  
+  
 })
 export class AppComponent {
   title = 'cp-and-p-updated';
--- src/app/room/room.service.ts
+++ src/app/room/room.service.ts
@@ -1,31 +1,35 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of, BehaviorSubject } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
-import { FileHandlerService } from '../services/file-handler.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService {
 
-  constructor(private fileHandlerService:FileHandlerService){}
-
   tempDetails:any;
 
+  private roomDetailsAllSubject = new BehaviorSubject<any>([]);
   private roomSubject = new BehaviorSubject<any>([]);
-  room$ = this.roomSubject.asObservable();
-
   private stateSubject = new BehaviorSubject<any>({});
+  
+  room$ = this.roomSubject.asObservable();
   state$ = this.stateSubject.asObservable();
 
   getRoom(): Observable<any[]> {
-    this.getFromFile()
+    // this.getFromFile()
     return this.roomSubject.asObservable();
   }
 
   setRoom(room: any[]): void {
     this.roomSubject.next(room);
+  }
+
+  getRoomDetailsAll(): Observable<any[]> {
+    return this.roomDetailsAllSubject.asObservable();
+  }
+
+  setRoomDetailsAll(roomDetails: any[]): void {
+    this.roomDetailsAllSubject.next(roomDetails);
   }
 
   getState(): any {
@@ -51,21 +55,12 @@ export class RoomService {
     { id: '3', userId: 'user3' }
   ]; // In-memory data store
 
-  // constructor(private http: HttpClient) {
-  //   // Load initial data from JSON file
-  //   this.loadRooms();
-  // }
-
   // Get all rooms from the rooms
   getRooms() {
     return this.rooms;
     
   }
 
-  getFromFile(){
-    this.tempDetails = this.fileHandlerService.getData()
-    console.log("retrieved data", this.tempDetails)
-  }
 
   getRoomDetails(userId:any) {
     const room = this.roomDetails.find(room => room.userId === userId);
@@ -85,19 +80,13 @@ export class RoomService {
     else{
     const newRoom = { id: this.generateId(), userId };
     this.rooms.push(newRoom);
+    // this.saveRooms();
     console.log("New Room Created",newRoom)
     }
 
     
   }
 
-  // Enter a room by id
-  // enterRoom(roomId: string) {
-  //   const room = this.rooms.find(room => room.id === roomId);
-  //   return of(room).pipe(
-  //     catchError(this.handleError<any>('enterRoom'))
-  //   );
-  // }
 
   // // Enter a room by userId
     enterRoom(userId: string) {
@@ -112,20 +101,7 @@ export class RoomService {
     return room
   }
 
-  // // Load rooms from mock JSON file
-  // private loadRooms(): void {
-  //   this.getRooms().subscribe(rooms => {
-  //     this.rooms = rooms || [];
-  //   });
-  // }
 
-  // Simulate saving rooms to the mock JSON file
-  // private saveRooms(): Observable<any> {
-  //   const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-  //   return this.http.put(this.roomsUrl, this.rooms, { headers }).pipe(
-  //     catchError(this.handleError<any>('saveRooms'))
-  //   );
-  // }
 
   // Generate a unique id for new rooms
   private generateId(): string {
